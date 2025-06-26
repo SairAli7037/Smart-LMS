b7f832f2d6e90f72b6dcec57ed84cfb1677fc6f9
Sair Ali <sairali7037@gmail.com>
2025-06-26 13:26:16 +0530

    update app path in settings

--- smart_lms/smart_lms/settings.py
+++ smart_lms/smart_lms/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os,sys
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.insert(0, str(BASE_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
