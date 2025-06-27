314a6449b9af004c7ed1c16dc7594534ed46b753
Sair Ali <sairali7037@gmail.com>
2025-06-27 11:37:07 +0530

    setting update

--- smart_lms/smart_lms/settings.py
+++ smart_lms/smart_lms/settings.py
@@ -53,9 +53,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
