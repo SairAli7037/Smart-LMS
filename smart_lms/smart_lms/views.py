ae6c42e657e765248d6389ad04761b308a6d3f53
Sair Ali <sairali7037@gmail.com>
2025-07-02 15:20:47 +0530

    update index view

--- smart_lms/smart_lms/views.py
+++ smart_lms/smart_lms/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
 
-@csrf_protect
+
 def serve_react(request):
     return render(request, 'index.html')
