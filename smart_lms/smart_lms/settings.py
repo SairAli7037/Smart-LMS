d231e66d356c5665b1a56deb691d6203df4ef676
Sair Ali <sairali7037@gmail.com>
2025-07-02 17:06:15 +0530

    remove templates

--- smart_lms/smart_lms/settings.py
+++ smart_lms/smart_lms/settings.py
@@ -103,7 +103,7 @@ ROOT_URLCONF = 'smart_lms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -168,12 +168,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/assets/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'Templates','assets')
-
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = []
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
